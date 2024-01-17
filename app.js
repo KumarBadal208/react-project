2daed11096eaa810a67db9214c778edbb291b61b
KumarBadal208 <kumarbadal208@gmail.com>
2024-01-17 21:21:38 +0530

    setup of react app

---
+++ app.js
@@ -0,0 +1,26 @@
+// --------****** REACT ----------------------------------------------------------------**************
+
+// -------** BASIC ****----------------
+
+let heading = React.createElement("h1", {
+    "id" : "header",
+    "className" : "head" // giving class as attribute
+}, "Hello from React");
+
+// header is just react element which is a javascript object. Its not the html tag yet.
+console.log(heading);
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); // render function takes the heading object and converts it into html heading tag and put it in the DOM tree.
+
+// **---------- Nested HTML tags ****-------
+
+let parent = React.createElement("div",{"id": "parent"},
+    React.createElement("div", {"id" : "child"}, [
+        React.createElement("h1", {}, "I am nested h1 heading"),
+        React.createElement("h2", {}, "I am nested h2 heading") // array of children
+    ]));
+
+console.log(parent);
+
+root.render(parent);
